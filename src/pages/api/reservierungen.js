f57f29eec2bf45f9dd9ce06fb8c3d2876999fd72
Amir Kaljikovic <office@foto-amir.com>
2025-05-17 18:56:16 +0200

    error bei import

--- src/pages/api/reservierungen.js
+++ src/pages/api/reservierungen.js
@@ -1,4 +1,4 @@
-import { supabase } from '../../lib/supaBaseClient';
+import { supabase } from '../../lib/supaBaseClient.js';
 
 export async function GET() {
   const { data, error } = await supabase.from('reservierung').select('*');
