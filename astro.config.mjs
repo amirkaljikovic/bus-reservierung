0cdc541f433023038da3a1da05a90e25bfacb1db
Amir Kaljikovic <office@foto-amir.com>
2025-05-17 13:07:38 +0200

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import react from '@astrojs/react';
+
 
 export default defineConfig({
   output: 'server',
+  integrations: [react()],
   adapter: node({ mode: 'standalone' }), // wichtig für Deployment
 });
